d74f6106a47c81f871266abd58ae7daa522080ef
Samarth Goyal <samarth_niku@yahoo.co.in>
2020-01-08 06:51:20 +0530

    Added Post Routes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
+const postsRouter = require('./routes/posts');
 const usersRouter = require('./routes/users');
 
 const app = express();
@@ -20,7 +21,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/posts', postsRouter);
 app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ routes/posts.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET posts index /posts */
+router.get('/', (req, res, next) => {
+    res.send('INDEX /posts');
+    // res.render('index', { title: 'Surf Shope - Home' });
+});
+
+/* GET posts new /posts/new */
+router.get('/new', (req, res, next) => {
+    res.send('NEW /posts/new');
+    // res.render('index', { title: 'Surf Shope - Home' });
+});
+
+/* POST posts create /posts */
+router.post('/', (req, res, next) => {
+    res.send('CREATE /posts');
+    // res.render('index', { title: 'Surf Shope - Home' });
+});
+
+/* GET posts show /posts/:id */
+router.get('/:id', (req, res, next) => {
+    res.send('SHOW /posts/:id');
+    // res.render('index', { title: 'Surf Shope - Home' });
+});
+
+/* GET posts edit /posts/:id/edit */
+router.get('/:id/edit', (req, res, next) => {
+    res.send('EDIT /posts/:id/edit');
+    // res.render('index', { title: 'Surf Shope - Home' });
+});
+
+/* PUT posts update /posts/:id */
+router.put('/:id', (req, res, next) => {
+    res.send('UPDATE /posts/:id');
+    // res.render('index', { title: 'Surf Shope - Home' });
+});
+
+/* DELETE posts destroy /posts/:id */
+router.delete('/:id', (req, res, next) => {
+    res.send('DELETE /posts/:id');
+    // res.render('index', { title: 'Surf Shope - Home' });
+});
+
+
+module.exports = router;
+
