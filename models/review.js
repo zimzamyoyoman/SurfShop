b74285da4fce834ded0b10b1b6bd1cfed7e888ba
Samarth Goyal <samarth_niku@yahoo.co.in>
2020-01-08 16:07:13 +0530

    Added Review Model

--- models/review.js
+++ models/review.js
@@ -1,3 +1,12 @@
-// REVIEW
-// -Body - String
-// -Author - Object ID ref User 
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ReviewSchema = new Schema({
+    body: String,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+});
+
+module.exports = mongoose.model('Review', ReviewSchema);
