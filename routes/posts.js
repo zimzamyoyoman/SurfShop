ea32129da0cb41f430076254b66cb1bb1b6190a1
Samarth Goyal <samarth_niku@yahoo.co.in>
2020-01-20 18:55:21 +0530

    Created new posts placeholder view

--- routes/posts.js
+++ routes/posts.js
@@ -1,45 +1,37 @@
 const express = require('express');
 const router = express.Router();
 const { errorHandler } = require('../middleware/index');
-const { getPosts } = require('../controllers/posts');
+const { getPosts, newPost } = require('../controllers/posts');
 
 /* GET posts index /posts */
 router.get('/', errorHandler(getPosts));
 
 /* GET posts new /posts/new */
-router.get('/new', (req, res, next) => {
-    res.send('NEW /posts/new');
-    // res.render('index', { title: 'Surf Shope - Home' });
-});
+router.get('/new', newPost);
 
 /* POST posts create /posts */
 router.post('/', (req, res, next) => {
     res.send('CREATE /posts');
-    // res.render('index', { title: 'Surf Shope - Home' });
 });
 
 /* GET posts show /posts/:id */
 router.get('/:id', (req, res, next) => {
     res.send('SHOW /posts/:id');
-    // res.render('index', { title: 'Surf Shope - Home' });
 });
 
 /* GET posts edit /posts/:id/edit */
 router.get('/:id/edit', (req, res, next) => {
     res.send('EDIT /posts/:id/edit');
-    // res.render('index', { title: 'Surf Shope - Home' });
 });
 
 /* PUT posts update /posts/:id */
 router.put('/:id', (req, res, next) => {
     res.send('UPDATE /posts/:id');
-    // res.render('index', { title: 'Surf Shope - Home' });
 });
 
 /* DELETE posts destroy /posts/:id */
 router.delete('/:id', (req, res, next) => {
     res.send('DELETE /posts/:id');
-    // res.render('index', { title: 'Surf Shope - Home' });
 });
 
 module.exports = router;
