e79c099b5a521df10428e4d2ffe8107cc29dea6d
Samarth Goyal <samarth_niku@yahoo.co.in>
2020-01-21 15:26:44 +0530

    Setup image upload with cloudinary

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,4 +1,10 @@
 const Post = require('../models/post');
+const cloudinary = require('cloudinary');
+cloudinary.config({
+    cloud_name: 'ds8vbkjfh',
+    api_key: '856418851593256',
+    api_secret: process.env.CLOUDINARY_SECRET
+});
 
 module.exports = {
 
@@ -15,6 +21,14 @@ module.exports = {
 
     // Posts Create 
     async postCreate(req, res, next) {
+        req.body.post.images = [];
+        for (const file of req.files) {
+            let image = await cloudinary.v2.uploader.upload(file.path);
+            req.body.post.images.push({
+                url: image.secure_url,
+                public_id: image.public_id
+            });
+        }
         let post = await Post.create(req.body.post);
         res.redirect(`/posts/${post.id}`);
     },
