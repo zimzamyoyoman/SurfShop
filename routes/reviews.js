b9a48d6df56dc03a0c2c531d661fafe489654a6d
Samarth Goyal <samarth_niku@yahoo.co.in>
2020-02-26 09:34:20 +0530

    Added deletion of reviews

--- routes/reviews.js
+++ routes/reviews.js
@@ -14,6 +14,6 @@ router.post('/', asyncErrorHandler(reviewCreate));
 router.put('/:review_id', isReviewAuthor, asyncErrorHandler(reviewUpdate));
 
 /* DELETE reviews destroy /posts/:id/reviews/:review_id */
-router.delete('/:id',asyncErrorHandler(reviewDestroy));
+router.delete('/:review_id', isReviewAuthor, asyncErrorHandler(reviewDestroy));
 
 module.exports = router;
